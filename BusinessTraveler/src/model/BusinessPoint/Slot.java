c7f723489c639c32369ebeb92d8435e66a1f7f44
yun_cn <zzwyxl@163.com>
2019-12-19 23:33:46 +0800

    将slot抽离

---
+++ BusinessTraveler/src/model/BusinessPoint/Slot.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.BusinessPoint;
+
+import model.Goods.Goods;
+
+/**
+ *
+ * @author Yun_c
+ */
+class Slot {
+    
+    private Goods good;
+    private int num;
+
+    public Slot(Goods good, int num) {
+        this.good = good;
+        this.num = num;
+    }
+
+    public int addNum(int n) {
+        this.num += n;
+        return this.num;
+    }
+
+    public int minusNum(int n) {
+        this.num -= n;
+        if (this.num < 0) {
+            this.num = 0;
+        }
+        return this.num;
+    }
+
+    public int setNum(int n) {
+        this.num = n;
+        return this.num;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public boolean equals(Object o) {
+        return good.equals(o);
+    }
+    
+}
--- BusinessTraveler/src/model/BusinessPoint/Store.java
+++ BusinessTraveler/src/model/BusinessPoint/Store.java
@@ -129,39 +129,3 @@ public class Store implements Tradable, Subject, EnvironmentInfluencable {
     }
 }
 
-class Slot {
-
-    private Goods good;
-    private int num;
-
-    public Slot(Goods good, int num) {
-        this.good = good;
-        this.num = num;
-    }
-
-    public int addNum(int n) {
-        this.num += n;
-        return this.num;
-    }
-
-    public int minusNum(int n) {
-        this.num -= n;
-        if (this.num < 0) {
-            this.num = 0;
-        }
-        return this.num;
-    }
-
-    public int setNum(int n) {
-        this.num = n;
-        return this.num;
-    }
-
-    public int getNum() {
-        return this.num;
-    }
-
-    public boolean equals(Object o) {
-        return good.equals(o);
-    }
-}
