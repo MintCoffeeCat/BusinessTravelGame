776ec9b9903ab2a2ef238d37f2f859648828c213
yun_cn <zzwyxl@163.com>
2019-12-21 01:23:20 +0800

    画路能自动寻找最适合的两个点进行连接

--- BusinessTraveler/src/component/DrawablePanel.java
+++ BusinessTraveler/src/component/DrawablePanel.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package component;
+
 import controller.MapDragController;
 import java.awt.BasicStroke;
 import java.awt.Graphics;
@@ -22,31 +23,71 @@ import model.WorldMap.WorldMap;
  *
  * @author Yun_c
  */
-public class DrawablePanel extends JPanel{
-    
-    public DrawablePanel(){
+public class DrawablePanel extends JPanel {
+
+    public DrawablePanel() {
         super();
     }
-    
+
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         WorldMap wMap = WorldMap.getInstance();
         for (Path p : wMap.getAllPaths()) {
             BusinessPointOnMap tempA = this.businessPoints.get(p.getA().getId());
             BusinessPointOnMap tempB = this.businessPoints.get(p.getB().getId());
-
+            int aX = tempA.getX();
+            int bX = tempB.getX();
+            int aY = tempA.getY();
+            int bY = tempB.getY();
             Graphics2D g2d = (Graphics2D) g.create();
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             BasicStroke bs1 = new BasicStroke(5);       // 笔画的轮廓（画笔宽度/线宽为5px）
             g2d.setStroke(bs1);
             g2d.setColor(p.getColor());
-            g2d.drawLine(tempA.getX(), tempA.getY(), tempB.getX(), tempB.getY());
+            int startX = 0;
+            int startY = 0;
+            int endX = 0;
+            int endY = 0;
+            if ((((aY < bY + tempB.getHeight()) && aY > bY) || ((aY + tempA.getHeight() > bY) && aY < bY))
+                    && ((aX > bX + tempB.getWidth()) || (aX + tempA.getWidth() < bX))) {
+                startX = aX > bX ? bX + tempB.getWidth() : aX + tempA.getWidth();
+                endX = aX > bX ? aX : bX;
+                startY = aX > bX ? (bY + bY + tempB.getHeight()) / 2 : (aY + aY + tempA.getHeight()) / 2;
+                endY = aX > bX ? (aY + aY + tempB.getHeight()) / 2 : (bY + bY + tempB.getHeight()) / 2;
+            } else if ((((aX < bX + tempB.getWidth()) && aX > bX) || ((aX + tempA.getWidth() > bX) && aX < bX))
+                    && ((aY > bY + tempB.getHeight()) || (aY + tempA.getHeight() < bY))) {
+                startX = aY > bY ? (bX + bX + tempB.getWidth()) / 2 : (aX + aX + tempA.getWidth()) / 2;
+                endX = aY > bY ? (aX + aX + tempA.getWidth()) / 2 : (bX + bX + tempB.getWidth()) / 2;
+                startY = aY > bY ? bY + tempB.getHeight() : aY + tempA.getHeight();
+                endY = aY > bY ? aY : bY;
+
+            } else if ((aX + tempA.getWidth() <= bX) && (aY >= bY + tempB.getHeight())) {
+                startX = aX + tempA.getWidth();
+                startY = aY;
+                endX = bX;
+                endY = bY + tempB.getHeight();
+            } else if ((aX >= bX + tempB.getWidth()) && (aY >= bY + tempB.getHeight())) {
+                startX = aX;
+                startY = aY;
+                endX = bX + tempB.getWidth();
+                endY = bY + tempB.getHeight();
+            } else if ((aX >= bX + tempB.getWidth()) && (aY + tempA.getHeight() <= bY)) {
+                startX = aX;
+                startY = aY + tempA.getHeight();
+                endX = bX + tempB.getWidth();
+                endY = bY;
+            } else {
+                startX = aX + tempA.getWidth();
+                startY = aY + tempA.getHeight();
+                endX = bX;
+                endY = bY;
+            }
+            g2d.drawLine(startX, startY, endX, endY);
 
         }
     }
-    
-    
+
     private Map<Integer, BusinessPointOnMap> businessPoints;
 
     public void initWorldMap(BusinessPoint[] bps, Path[] paths) {
